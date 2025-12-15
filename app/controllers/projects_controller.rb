0a28a5ba3c9c0fdc076a7038889be53c5305d258
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-15 14:59:47 +0900

    Def create method & strong params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,17 @@
 class ProjectsController < ApplicationController
   def create
+    @project = current_user.projects.build(project_params)
+
+    if @project.save
+      redirect_to root_path, notice: "Project uploaded successfully!"
+    else
+      redirect_to root_path, alert: "Upload failed: #{@project.errors.full_messages.join(', ')}"
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :file)
   end
 end
