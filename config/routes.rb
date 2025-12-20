66cbaa0147532dd8d2461d3c67fc7bd02f50bdf7
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-20 17:13:45 +0900

    Create Project show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Projects
-  resources :projects, only: [:create, :destroy] do
+  # Create: Upload new project (ZIP or single file)
+  # Show: View extracted contents of a project (Look inside)
+  # Destroy: Delete a project (and it's contents)
+  # Download: Download the original uploaded file
+  resources :projects, only: [:create, :show, :destroy] do
     member do
       get :download
     end
   end
 
-  # Library
+  # Library - main dashboard showing all user's files
   get 'library/index'
   root "library#index"
 
