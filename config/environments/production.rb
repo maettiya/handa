ac8e06988913b69bf39b53f560b816c2095204af
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-23 19:50:13 +0900

    Disable Active Storage checksum for R2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudflare
 
+  # Disable Active Storage checksum for R2 compatibility
+  config.active_storage.checksum_implementation = nil
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
