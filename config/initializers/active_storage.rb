991b4beee882142cfe6d92b492659edd6245b8ca
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-23 19:59:31 +0900

    Require aws-sdk-s3 before configuring

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,6 +1,8 @@
 # Disable request checksum for Cloudflare R2 compatibility
 # R2 doesn't support the newer S3 checksum algorithms
 
+require "aws-sdk-s3"
+
 Aws.config.update(
   request_checksum_calculation: "when_required",
   response_checksum_validation: "when_required"
