4a1628ac7b90d05d51f261d1188b2bb8e54ef4cf
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-23 19:57:27 +0900

    Configure AWS SDK checksum settings for R2

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,19 +1,7 @@
-# Disable checksum for Cloudflare R2 compatibility
+# Disable request checksum for Cloudflare R2 compatibility
 # R2 doesn't support the newer S3 checksum algorithms
 
-Rails.application.config.after_initialize do
-  if defined?(ActiveStorage::Service::S3Service)
-    ActiveStorage::Service::S3Service.class_eval do
-      def upload(key, io, checksum: nil, **options)
-        instrument :upload, key: key, checksum: checksum do
-          @client.put_object(
-            bucket: @bucket.name,
-            key: key,
-            body: io,
-            **options
-          )
-        end
-      end
-    end
-  end
-end
+Aws.config.update(
+  request_checksum_calculation: "when_required",
+  response_checksum_validation: "when_required"
+)
