895c78bec84427791c65cb0d2035b42d59e0b1d8
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-28 12:24:06 +0900

    Edit javascript for live user avatar update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./upload"
+
+// Auto-submit avatar form when file is selected
+document.addEventListener("turbo:load", () => {
+  const avatarInput = document.getElementById("avatar-input");
+  if (avatarInput) {
+    avatarInput.addEventListener("change", () => {
+      avatarInput.closest("form").requestSubmit();
+    });
+  }
+});
