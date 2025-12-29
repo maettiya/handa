43d5a9e6ee31b01ee9baf1505b5c8bcde9903530
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-29 14:19:45 +0900

    Add callback to Project model to catch non-zipped file uploads

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,36 @@ class Project < ApplicationRecord
   validates :title, presence: true
   validates :file, presence: true, unless: :folder?
 
+  before_save :detect_project_type, if: -> { file.attached? && project_type.blank? }
+
   def folder?
     project_type == "folder"
+  end
+
+  private
+
+  def detect_project_type
+    return unless file.attached?
+
+    ext = File.extname(file.filename.to_s).delete('.').downcase
+
+    self.project_type = case ext
+    when 'als'
+      'ableton'
+    when 'logicx'
+      'logic'
+    when 'flp'
+      'fl_studio'
+    when 'ptx'
+      'pro_tools'
+    when 'wav', 'aif', 'aiff', 'flac'
+      'lossless_audio'
+    when 'mp3', 'm4a', 'aac', 'ogg'
+      'compressed_audio'
+    when 'zip'
+      nil  # Let extraction service handle ZIPs
+    else
+      'other'
+    end
   end
 end
