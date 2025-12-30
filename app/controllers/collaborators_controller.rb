0dfafeea5b1dfb34316c866174e3b954e248b6dc
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-30 13:20:40 +0900

    Def logic to find another user by username, add them & flash alerts for invalid adds

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -12,5 +12,20 @@ class CollaboratorsController < ApplicationController
       flash[:alert] = "Please enter a username"
       redirect_to collaborators_path and return
     end
+
+    collaborator = User.find_by("LOWER(username) = ?", username)
+
+    if collaborator.nil?
+      flash[:alert] = "User '#{params[:username]}' not found"
+    elsif collaborator == current_user
+      flash[:alert] = "You can't add yourself as a collaborator"
+    elsif current_user.collaborators.include?(collaborator)
+      flash[:alert] = "#{collaborator.username} is already a collaborator"
+    else
+      Collaboration.create!(user: current_user, collaborator: collaborator)
+      flash[:notice] = "#{collaborator.username} added as a collaborator"
+    end
+
+    redirect_to collaborators_path
   end
 end
