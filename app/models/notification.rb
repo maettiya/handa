0b60a7c91a6fc7bf1805ddb5be801bff042b8ac7
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-30 14:56:16 +0900

    Add scopes & validations to Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,10 @@
 class Notification < ApplicationRecord
   belongs_to :user      # The recipient
   belongs_to :actor, class_name: 'User'     # The person who triggered the notification
+  belongs_to :notifiable, polymorphic: true, optional: true     #Able to link to the associated record
 
   scope :unread, -> { where(read: false) }
-  scope :recent
+  scope :recent, -> { order(created_at: :desc) }
+
+  validates "notification_type, presence: true"
 end
