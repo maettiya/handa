f15b9b9992c04816414f77c0dc9370070c081be2
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-30 12:57:27 +0900

    Def private method for preventing user adding self as a collaborator

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -4,4 +4,12 @@ class Collaboration < ApplicationRecord
 
   validate :not_self_collaboration
   validate :mutual_uniqueness
+
+  private
+
+  def not_self_collaboration
+    errors.add(:collaborator, "can't be yourself") if user_id == collaborator_id
+  end
+
+
 end
