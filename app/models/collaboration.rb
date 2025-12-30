9e5b4dd4f033175bd5f94323141265f0c3e06ae8
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-30 12:55:54 +0900

    Add validatons to Collaboration model

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,4 +1,7 @@
 class Collaboration < ApplicationRecord
   belongs_to :user
-  belongs_to :collaborator
+  belongs_to :collaborator, class_name: 'User'
+
+  validate :not_self_collaboration
+  validate :mutual_uniqueness
 end
