55ee970a191af885794049d5e7b81bec595fba64
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-31 16:07:14 +0900

    Edit library to filter out ephemeral projects

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,6 +1,6 @@
 class LibraryController < ApplicationController
   def index
     # Order by most recently created first (newest uploads/folders appear first)
-    @projects = current_user.projects.order(created_at: :desc)
+    @projects = current_user.library_projects
   end
 end
