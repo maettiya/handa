e080ffba00c63d19f7df243deb39cd27b51aac1b
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-31 12:07:15 +0900

    Edit Share Link model & add validations

--- app/models/share_link.rb
+++ app/models/share_link.rb
@@ -1,3 +1,8 @@
 class ShareLink < ApplicationRecord
   belongs_to :project
+
+  has_secure_password validations: false
+
+  # Generate unique token before creation
+  before_create :generate_token
 end
