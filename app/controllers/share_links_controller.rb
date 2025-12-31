0353fe1d3a1d61ecab1088bdc5b99a1ad2c5ba62
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-31 12:30:21 +0900

    Share link creation success and failure redirection & message

--- app/controllers/share_links_controller.rb
+++ app/controllers/share_links_controller.rb
@@ -7,6 +7,16 @@ class ShareLinksController < ApplicationController
   # Creates a new share link for a project (requires login)
   def create
     @share_link = @project.share_links.new(share_link_params)
+
+    if @share_link.save
+      render json: {
+        success: true,
+        url: share_link_url(@share_link.token),
+        token: @share_link.token
+      }
+    else
+      render json: { success: false, errors: @share_link.errors.full_messages }, status: unprocessable_entity
+    end
   end
 
 end
