67e26a042e436acd6864672b232efb97299a9c93
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-31 12:27:27 +0900

    Def create method and share link creation logic

--- app/controllers/share_links_controller.rb
+++ app/controllers/share_links_controller.rb
@@ -3,6 +3,10 @@ class ShareLinksController < ApplicationController
   before_action :set_share_link, only: [:show, :download, :verify_password]
   before_action :set_project, only: [:create]
 
-
+  # POST /projects/:project_id/share_links
+  # Creates a new share link for a project (requires login)
+  def create
+    @share_link = @project.share_links.new(share_link_params)
+  end
 
 end
