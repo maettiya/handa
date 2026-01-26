195abb435cccb142ac4aec8e38aff0880937269f
Matt McLuckie <mcluckiematt@gmail.com>
2026-01-26 11:44:20 +0900

    Update migration file & foreign keys

--- db/migrate/20260126024242_create_downloads.rb
+++ db/migrate/20260126024242_create_downloads.rb
@@ -3,13 +3,15 @@ class CreateDownloads < ActiveRecord::Migration[7.1]
     create_table :downloads do |t|
       t.references :user, null: false, foreign_key: true
       t.references :asset, null: false, foreign_key: true
-      t.string :status
-      t.integer :progress
-      t.integer :total
+      t.string :status, default: 'pending', null: false
+      t.integer :progress, default: 0
+      t.integer :total, default: 0
       t.string :filename
       t.string :error_message
 
       t.timestamps
     end
+
+    add_index :downloads, [:user_id, :status]
   end
 end
